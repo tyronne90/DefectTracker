1f60e873afc83b61efb9a2a72bac384bdfb6208f
Tyron <tyronne90@gmail.com>
2019-03-20 01:25:14 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+//doughnut
+var ctxD = document.getElementById("doughnutChart").getContext("2d");
+var myLineChart = new Chart(ctxD, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+    datasets: [
+      {
+        data: [300, 50, 100, 40, 120],
+        backgroundColor: [
+          "#F7464A",
+          "#46BFBD",
+          "#FDB45C",
+          "#949FB1",
+          "#4D5360"
+        ],
+        hoverBackgroundColor: [
+          "#FF5A5E",
+          "#5AD3D1",
+          "#FFC870",
+          "#A8B3C5",
+          "#616774"
+        ],
+        hoverBorderWidth: [1, 5, 1, 1],
+        hoverBorderColor: ["black", "black", "black", "black"]
+      }
+    ]
+  },
+  options: {
+    responsive: true
+  }
+});
+
+//pie
+var ctxP = document.getElementById("pieChart").getContext("2d");
+var myPieChart = new Chart(ctxP, {
+  type: "pie",
+  data: {
+    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+    datasets: [
+      {
+        data: [300, 50, 100, 40, 120],
+        backgroundColor: [
+          "#F7464A",
+          "#46BFBD",
+          "#FDB45C",
+          "#949FB1",
+          "#4D5360"
+        ],
+        hoverBackgroundColor: [
+          "#FF5A5E",
+          "#5AD3D1",
+          "#FFC870",
+          "#A8B3C5",
+          "#616774"
+        ]
+      }
+    ]
+  },
+  options: {
+    responsive: true
+  }
+});
+
+// popovers initialization - on hover
+$('[data-toggle="popover-hover"]').popover({
+  html: true,
+  trigger: "hover",
+  placement: "bottom",
+  content: function() {
+    return '<img src="' + $(this).data("img") + '" />';
+  }
+});
